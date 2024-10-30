24c6a690a45d2ea16ad668dfa1789a644f22c86a
shrestharonit09 <stharonit09@gmail.com>
2024-10-30 12:26:13 +0545

    responsive working

--- src/components/Project.js
+++ src/components/Project.js
@@ -35,7 +35,7 @@ const Project = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="w-[50vw] sm:w-[40vw] md:w-[27vw] xl:w-[60vw] grid grid-cols-1 xl:grid-cols-3 gap-3 sm:gap-5 md:gap-5 lg:gap-10 ">
+        <div className="w-[55vw] sm:w-[42vw] md:w-[68vw] lg:w-[55vw] xl:w-[70vw] 2xl:w-[65vw]  grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-20 sm:gap-24 md:gap-8 lg:gap-8 xl:gap-12">
           {student.map((item) => (
             <Card
               title={item.title}
